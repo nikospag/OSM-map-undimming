b2a67cd2820266b62dd9f724c75fa71fd8d0014d
nikospag <nikospag2@gmail.com>
2024-11-27 05:12:51 +0200

    Update OSM-map-undimming.user.js

--- OSM-map-undimming.user.js
+++ OSM-map-undimming.user.js
@@ -1,1 +1,18 @@
+// ==UserScript==
+// @name         Remove map dimming in dark mode for OpenStreetMap
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Remove map dimming in dark mode for OpenStreetMap
+// @author       nikospag
+// @match        https://www.openstreetmap.org/*
+// @grant        GM_addStyle
+// ==/UserScript==
 
+
+GM_addStyle(`
+    @media(prefers-color-scheme: dark) {
+        .leaflet-tile-container, .leaflet-tile {
+            filter:brightness(1) !important
+        }
+    }
+`);
